c6b3d8c2c498605f8e936877908b7f50750e7fbd
Nicolas Araújo <nicolasaraujo.sj@gmail.com>
2020-11-01 20:53:41 -0300

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,6 @@
+# 1º Print a board.
+# 2º Take in player input.
+# 3º Place their input on the board.
+# 4º Check if the game is won,tied, lost, or ongoing.
+# 5º Repeat 2º and 3ª until the game has been won or tied.
+# 6º Ask if players want to play again.
