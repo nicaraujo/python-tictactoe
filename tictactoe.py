3b3265b433c2ed48a23916adbf26ce447ba080be
Nicolas Araújo <nicolasaraujo.sj@gmail.com>
2020-11-02 15:43:55 -0300

    :sparkles: New function that defines players markers

--- tictactoe.py
+++ tictactoe.py
@@ -16,10 +16,25 @@ def board():
     print('|       |       |       |')
     print('=========================')
 
-
 # 2º Take in player input.
-# 3º Place their input on the board.
-# 4º Check if the game is won,tied, lost, or ongoing.
-# 5º Repeat 2º and 3ª until the game has been won or tied.
-# 6º Ask if players want to play again.
+
+
+def player_input():
+    '''This function collects player one marker choice and assign a marker to player two based on player one marker choice'''
+    marker = ''
+    while marker != 'X' and marker != 'O':
+        print('Player One, please choose a marker between X or O to begin your game:')
+        marker = input()
+    p1 = marker
+    if p1 == 'X':
+        p2 = 'O'
+    else:
+        p2 = 'X'
+    print(f'Player One choose {p1}, so Player Two your marker is {p2}')
+
+    # 3º Place their input on the board.
+    # 4º Check if the game is won,tied, lost, or ongoing.
+    # 5º Repeat 2º and 3ª until the game has been won or tied.
+    # 6º Ask if players want to play again.
 board()
+player_input()
