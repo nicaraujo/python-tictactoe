6615a1f81fc79d00e183deb8977381df27408188
Nicolas Araújo <nicolasaraujo.sj@gmail.com>
2020-11-04 19:06:18 -0300

    :sparkles: New function that checks if the game is won

--- tictactoe.py
+++ tictactoe.py
@@ -13,10 +13,12 @@ def board(b):
     '''Prints the board to the players'''
     clear()
 #    b = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    print('   #TIC #TAC #TOE BOARD')
+    print('\n')
+    print('  #TIC #TAC #TOE BOARD')
+    print('\n')
     print('=========================')
     print('|       |       |       |')
-    print(f'|   {b[1]}   |   {b[2]}   |   {b[3]}   |')
+    print(f'|   {b[1]}   |   {b[1]}   |   {b[3]}   |')
     print('|       |       |       |')
     print('=========================')
     print('|       |       |       |')
@@ -27,6 +29,8 @@ def board(b):
     print(f'|   {b[7]}   |   {b[8]}   |   {b[9]}   |')
     print('|       |       |       |')
     print('=========================')
+    print('\n')
+
 
    # return b
 test = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -34,37 +38,49 @@ board(test)
 
 
 def player_input():
-    '''Collects player one marker choice and assign a marker to player two based on player one marker choice'''
+    '''
+    Collects player one marker choice and assign a marker to player two based on player one marker choice
+    '''
     marker = ''
-    while marker != 'X' and marker != 'O':
+    while not (marker == 'X' or marker == 'O'):
         print('Player One, please choose a marker between X or O to begin your game:')
-        marker = input()
+        marker = input().upper()
         if marker != 'X' or marker != 'O':
             print('Wrong choice!')
-    p1 = marker
-    if p1 == 'X':
-        p2 = 'O'
+    if marker == 'X':
+        return ('X', 'O')
     else:
-        p2 = 'X'
-    print(f'Player One chose {p1}, so Player Two your marker is {p2}')
-    return (p1, p2)
+        return ('O', 'X')
 
 
 p1_marker, p2_marker = player_input()
 
 
-def place_marker(test, marker):
-    '''Places the player marker in the the selected board index'''
+def place_marker(test, marker, position):
+    '''
+    Places the player marker in the the selected board index
+    '''
     position = int(
         input('Enter a position to from 1 to 9 for assign your marker: '))
     test[position] = marker
-    return test
 
-# 4º Check if the game is won,tied, lost, or ongoing.
+
+def win_check(board, marker):
+    '''
+    Checks if the game is won
+    '''
+    return ((board[1] == board[2] == board[3] == marker) or
+            (board[4] == board[5] == board[6] == marker) or
+            (board[7] == board[8] == board[9] == marker) or
+            (board[1] == board[4] == board[7] == marker) or
+            (board[2] == board[5] == board[8] == marker) or
+            (board[3] == board[6] == board[9] == marker) or
+            (board[1] == board[5] == board[9] == marker) or
+            (board[3] == board[5] == board[7] == marker))
+
 # 5º Repeat 2º and 3ª until the game has been won or tied.
 # 6º Ask if players want to play again.
 
 
 board(test)
-place_marker(test, 'X')
 board(test)
